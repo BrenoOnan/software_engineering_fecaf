4da53ffdb7c9da77973a461239db781856112833
Breno Camargo <211529127+BrenoOnan@users.noreply.github.com>
2026-07-27 20:02:26 -0300

    Organizando e revisando o código
    foi adicionado melhorias para deixar mais limpo o código, e adicionado cores

--- funcoes/func_crud.py
+++ funcoes/func_crud.py
@@ -1,3 +1,4 @@
+from rich import print
 import json
 
 def abrir_arquivo(estoque): #verifica se existe o arquivo e tenta abrir 
@@ -14,15 +15,14 @@ def criar_arquivo(estoque): #cria o arquivo caso não haja arquivo
             pass
         
     except Exception as error:
-        print({error})
-
+        print(f'{error} ❌')
     else:
         print('Arquivo gerado com sucesso')
         
-        
+
 def cabecalho(): #cabeçalho da loja#
     print("--"*19)
-    print('Eletronicos full '.center(35))
+    print('[blue]Eletronicos full[/]'.center(45))
     print("--"*19)
     
 
@@ -40,15 +40,11 @@ def menu_opcoes(estoque): #função para abrir o menu de opçoes
     try:
         while True:
             from time import sleep
-            cabecalho()
-            sleep(1)
-            print("1 - Ver estoque")
-            sleep(1)
-            print("2 - Atualizar produto")
-            sleep(1)
-            print("3 - Adicionar produto")
-            sleep(1)
-            print("4 - Deletar produto")
+            cabecalho(), sleep(1)
+            print("1 - Ver estoque"), sleep(1)
+            print("2 - Atualizar produto"), sleep(1)
+            print("3 - Adicionar produto"), sleep(1)
+            print("4 - Deletar produto"), sleep(1)
             print('--'*16)
             opcao = str(input('Qual das opçoes[Enter para sair]:')).strip()
             if opcao == '':
@@ -65,7 +61,7 @@ def menu_opcoes(estoque): #função para abrir o menu de opçoes
                 print('Opção invalida!')  
                 
     except Exception as error:
-        print(error)
+        print(f'{error} ❌')
         
 
 def ver_estoque(estoque): #função para printar o estoque de forma tabular 
@@ -73,16 +69,16 @@ def ver_estoque(estoque): #função para printar o estoque de forma tabular
         from time import sleep
         with open(estoque, 'r', encoding="utf-8")  as arq:
            estoque_produtos = json.load(arq) 
-           print('-='*22)
+           print('-'*49)
            print(f'{"ID":<5} {"Produto":<15} {"Quantidade":<14} {"Preço":<10}')
-           print('-'*44)
+           print('-'*49)
            
            for id, keys in estoque_produtos.items():
                sleep(1)
                print(f"{id:<5} {keys['nome']:<15} {keys['quantidade']:<13} R$:{keys['preço']:<10.2f}")
                
     except Exception as error:
-        print(error)
+        print(f'{error} ❌')
     
     
 def atualizar_produto(estoque): #função para atualizar o produto do estoque
@@ -92,32 +88,30 @@ def atualizar_produto(estoque): #função para atualizar o produto do estoque
             estoque_produtos = json.load(arq)
 
             user = (str(input('Digite o produto para atualizar[ID:]:'))).strip()
-            print('Proucurando...')
-            sleep(1)
+            print('Proucurando...'), sleep(1)
+            
             if user in estoque_produtos:
-                print(f'Produto selecionado: {estoque_produtos[user]['nome']}')
+                print(f'Produto selecionado: [blue]{estoque_produtos[user]['nome']}[/]')
                 
                 nova_quantidade= input('Qual a nova quantidade: ').strip()
-                
                 novo_preco = input('Qual o novo preço do item R$: ').strip()
                 
-                if nova_quantidade != '':
-                    estoque_produtos[user]['quantidade'] = int(nova_quantidade)
+                if nova_quantidade != '': #converte para int
+                    estoque_produtos[user]['quantidade'] += int(nova_quantidade)
                     
-                if novo_preco != '':
+                if novo_preco != '': #converte para int
                     estoque_produtos[user]['preço'] = int(novo_preco)
 
                 with open (estoque,'w', encoding='utf= 8') as arq:
                     json.dump(estoque_produtos, arq, indent= 4, ensure_ascii= False)
                 
-                print('Atualiazando...')
-                sleep(1)
-                print(f'{estoque_produtos[user]['nome']} atualizado com sucesso')
+                print('Atualiazando...'), sleep(1)
+                print(f'[blue]{estoque_produtos[user]['nome']}[/] atualizado com sucesso')
             else:
                 print('Produto não encontrado!')    
                 
     except Exception as error:
-        print(error)
+        print(f'{error} ❌')
 
 
 def adicionar_produto(estoque): #função para adicionar novo produto
@@ -130,9 +124,10 @@ def adicionar_produto(estoque): #função para adicionar novo produto
             if user == '':
                 print("Invalido!")
                 return
-            for linha in estoque_produto.values():
+            
+            for linha in estoque_produto.values(): #verifica se existe o produto no estoque
                     if linha["nome"] == user:
-                        print(f'{user} ja existe!')
+                        print(f'[blue]{user}[/] ja existe!')
                         return
             else:
                 add_quantidade = int(input("Qual a quantidade a entrar em estoque:"))
@@ -146,10 +141,10 @@ def adicionar_produto(estoque): #função para adicionar novo produto
             }
                 with open(estoque, 'w', encoding= "utf=8") as arq:
                     json.dump(estoque_produto, arq, indent= 4, ensure_ascii= False)
-                print(f'{user} adicionado com sucesso!')
+                print(f'[blue]{user}[/] adicionado com sucesso!')
 
     except Exception as error:
-        print(error)
+        print(f'{error} ❌')
 
 
 def deletar_produto(estoque): #função para encontrar deletar produto do estoque 
@@ -157,13 +152,13 @@ def deletar_produto(estoque): #função para encontrar deletar produto do estoqu
         from time import sleep
         with open(estoque, 'r', encoding="utf=8") as arq:
             estoque_produto = json.load(arq)
-            user = str(input('Qual produto deseja excluir do estoque[ID]:')).strip()
-            if user in estoque_produto:
-                print(f'Produto encontrado: {estoque_produto[user]["nome"]}')
+            id_user = str(input('Qual produto deseja excluir do estoque[ID]:')).strip()
+            if id_user in estoque_produto:
+                print(f'Produto encontrado: [blue]{estoque_produto[id_user]["nome"]}[/]')
                 print("Excluindo o produto...")
-                del estoque_produto[user]
-                sleep(3)
-                print("Excluido com sucesso!")
+                del estoque_produto[id_user]
+                sleep(2)
+                print(f"Excluido com sucesso ✅")
                 with open(estoque, 'w', encoding="utf=8") as arq:
                     json.dump(estoque_produto, arq, indent= 4, ensure_ascii=False)
                 
@@ -172,13 +167,13 @@ def deletar_produto(estoque): #função para encontrar deletar produto do estoqu
                 return
             
     except Exception as error:
-        print(error)
+        print(f'{error} ❌')
 
 def cliente_compras(estoque): #função cliente, comprar produtos
     while True:
         try:
             from time import sleep
-            ver_estoque(estoque)
+            ver_estoque(estoque), print()
             with open(estoque, 'r', encoding='utf=8') as arq:
                 estoque_produtos = json.load(arq)
                 
@@ -188,7 +183,7 @@ def cliente_compras(estoque): #função cliente, comprar produtos
                     break
                 
                 if produto_comprar in estoque_produtos:
-                    print(f"{estoque_produtos[produto_comprar]['nome']} selecionado")
+                    print(f"[blue]{estoque_produtos[produto_comprar]['nome']}[/] selecionado ✅")
                     
                     quantidade = int(input('Selecione a quantidade:'))
                     if quantidade < estoque_produtos[produto_comprar]['quantidade']:
@@ -197,9 +192,8 @@ def cliente_compras(estoque): #função cliente, comprar produtos
                         estoque_produtos[produto_comprar]['quantidade'] += - quantidade
                         
                         print(f'Valor da compra R$:{valor:.2f}')
-                        print(f'Confirmando a compra...')
-                        sleep(3)
-                        print(f'Compra efetuada, volte sempre')
+                        print(f'Confirmando a compra...'), sleep(2)
+                        print(f'Compra efetuada✅\nVolte sempre')
                         with open (estoque, 'w', encoding='utf=8') as arq:
                             json.dump(estoque_produtos, arq, indent=4, ensure_ascii=False)
                         break
@@ -207,8 +201,8 @@ def cliente_compras(estoque): #função cliente, comprar produtos
                     else:
                         print('Não temos essa quantidade em estoque\nTente novamente!')
                 else:
-                    print('Produto não encontrado, tente novamente')
+                    print('Produto não encontrado ❌, tente novamente')
                                     
             
         except Exception as error:
-            print(error)
+            print(f'{error} ❌')
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from rich import print
 from funcoes import func_crud
 import json
 from time import sleep
@@ -14,19 +15,16 @@ while True:
     print('[2] - Administrador')#sessão administrador
     user = str(input('Selecione as opçoes acima[Enter sair]: '))
     if user == '':
-        print('Encerrando...')
-        sleep(1)
+        print('Encerrando...'), sleep(1)
         break
     elif user == '1':
         func_crud.cliente_compras(arquivo) #abrindo sessão cliente
-        print('Encerrando...')
-        sleep(1)
+        print('Encerrando...'), sleep(1)
         break
     elif user == '2':
         senha = int(input('Qual a senha: '))
         func_crud.senha_adm(senha, arquivo) #abrindo sessão administrador
-        print('Encerrando...')
-        sleep(1)
+        print('Encerrando...'), sleep(1)
         break
     else:
         print('Opção inválida')
